a57d2a72d7008617133c03e762ca98f7cf7fd910
raneem6521 <148805278+raneem6521@users.noreply.github.com>
2023-11-15 19:41:31 -0700

    Update EffortLog.java

--- EffortLog.java
+++ EffortLog.java
@@ -1,80 +1,16 @@
 package application;
 
+//parent class for effort and defect logs
 public class EffortLog {
-    private static int numEffort;
-    private String project;
-    private String defectName;
-    private String defectSymptoms;
-    private String injected;
-    private String removed;
-    private String defectCategory;
-
-    // Getter and Setter for numEffort
-    public static int getNumEffort() {
-        return numEffort;
-    }
-
-    public static void setNumEffort(int numEffort) {
-        EffortLog.numEffort = numEffort;
-    }
-
-    // Getter and Setter for project
-    public String getProject() {
-        return project;
-    }
-
-    public void setProject(String project) {
-        this.project = project;
-    }
-
-    // Getter and Setter for defectName
-    public String getDefectName() {
-        return defectName;
-    }
-
-    public void setDefectName(String defectName) {
-        this.defectName = defectName;
-    }
-
-    // Getter and Setter for defectSymptoms
-    public String getDefectSymptoms() {
-        return defectSymptoms;
-    }
-
-    public void setDefectSymptoms(String defectSymptoms) {
-        this.defectSymptoms = defectSymptoms;
-    }
-
-    // Getter and Setter for injected
-    public String getInjected() {
-        return injected;
-    }
-
-    public void setInjected(String injected) {
-        this.injected = injected;
-    }
-
-    // Getter and Setter for removed
-    public String getRemoved() {
-        return removed;
-    }
-
-    public void setRemoved(String removed) {
-        this.removed = removed;
-    }
-
-    // Getter and Setter for defectCategory
-    public String getDefectCategory() {
-        return defectCategory;
-    }
-
-    public void setDefectCategory(String defectCategory) {
-        this.defectCategory = defectCategory;
-    }
-    
-    public String toString() {
-    	return "Num Effort: " + numEffort + "\nProject: " + project + "\nDefect Name: " + defectName + 
-    			"\nDefect Symptoms: " + defectSymptoms + "\nStep when Injected: " + injected + 
-    			"\nStep when Removed: " + removed + "\nDefect Category: " + defectCategory + "\n\n";
-    }
+   protected static int totalLogs = 0;
+   public static EffortLog[] logs = new EffortLog[995];
+   
+   public static void setTotalLogs(int logs) {
+	   totalLogs = logs;
+   }
+   
+   public static int getTotalLogs() {
+	   return totalLogs;
+   }
+   
 }
